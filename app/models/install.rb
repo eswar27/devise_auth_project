8a1174adb724bcf9e09e6128c8d1ce6cdb0a1bf0
Eswaramoorthy <eswaramoorthy.b@caratlane.com>
2023-03-22 11:59:04 +0530

    Revert "checking commit in feature brch"
    This reverts commit b803e411b6ccdbb01a8e9d71895c8d2d5a54ebad.

--- app/models/install.rb
+++ app/models/install.rb
@@ -3,5 +3,5 @@ class Install < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates_format_of :email, /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i
+  # validates_format_of :email, with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i
 end
