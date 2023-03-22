ccfca2e8f2806060e32c53bfd4c648068a9469b4
Eswaramoorthy <eswaramoorthy.b@caratlane.com>
2023-03-22 12:11:45 +0530

    new commit in feature branch

--- app/models/install.rb
+++ app/models/install.rb
@@ -1,7 +1,7 @@
 class Install < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates_format_of :email, /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i
 end
